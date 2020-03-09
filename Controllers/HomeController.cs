c5cf0f82fb42d0467a7df0ec05c15d6439d8eb29
Viktor Feldman <viktor.feldman@youngliving.com>
2020-03-09 13:29:49 -0600

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FrontierCodingCore.Models;
+using FrontierCodingTest.Services;
+using FrontierCodingTest.Models;
+
+namespace FrontierCodingCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly IAcountService _accountService;
+        public HomeController(IAcountService accountService)
+        {
+            _accountService = accountService;
+        }
+        public async Task<ActionResult> Index()
+        {
+            // IAcountService accountService = new AccountService();
+            var accounts = await _accountService.GetAccounts();
+            List<AccountsByStatus> accountsByStatus = _accountService.GetAccountsByStatus(accounts);
+            ViewBag.AccountsByStatuses = accountsByStatus;
+            return View();
+        }
+
+    
+    }
+}
